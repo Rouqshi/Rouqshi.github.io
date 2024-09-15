fb1c8fdd8be3e99ba90320b43dfc760b22cb0615
Rouqshi <142409572+Rouqshi@users.noreply.github.com>
2024-09-15 21:31:09 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const images = document.querySelectorAll("img");
+   for(const Image of images){
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(Response =>Response.json())
+    .then(data => {
+    Image.src = data.message
+    Image.width =100;
+    Image.height =100;
+    })
+   }
+    
+
+})
